d95bf8349754e3b95d6ef13615b6fd21a33856fa
joellokota@gmail.com <joellokota@gmail.com>
2024-11-19 16:40:03 +0100

    End of Microservice created

--- nathy-discovery/src/main/java/com/andyalex/nathydiscovery/NathyDiscoveryApplication.java
+++ nathy-discovery/src/main/java/com/andyalex/nathydiscovery/NathyDiscoveryApplication.java
@@ -2,7 +2,10 @@ package com.andyalex.nathydiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class NathyDiscoveryApplication {
 
--- nathy-gateway/src/main/java/com/andyalex/nathygateway/NathyGatewayApplication.java
+++ nathy-gateway/src/main/java/com/andyalex/nathygateway/NathyGatewayApplication.java
@@ -3,11 +3,27 @@ package com.andyalex.nathygateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.cloud.client.discovery.ReactiveDiscoveryClient;
+import org.springframework.cloud.gateway.discovery.DiscoveryClientRouteDefinitionLocator;
+import org.springframework.cloud.gateway.discovery.DiscoveryLocatorProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("*")
 @SpringBootApplication
 public class NathyGatewayApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(NathyGatewayApplication.class, args);
     }
+
+    @Bean
+    public DiscoveryClientRouteDefinitionLocator discoveryClientRouteDefinitionLocator(
+            ReactiveDiscoveryClient rdc,
+            DiscoveryLocatorProperties ldp){
+
+        return new DiscoveryClientRouteDefinitionLocator(rdc, ldp);
+    }
+
 
 }
